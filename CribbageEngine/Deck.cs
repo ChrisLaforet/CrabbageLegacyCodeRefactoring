4ab71c3828b840fd91c907f41218ae10e25336f0
Chris Laforet <laforet@chrislaforetsoftware.com>
2021-03-21 08:16:56 -0400

    Add project files.

---
+++ CribbageEngine/Deck.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CribbageEngine
+{
+    public class Deck
+    {
+        //CONSTANTS
+        public const int NumCards = 52;
+       
+        //CONSTRUCTOR
+        public Deck()
+        {
+
+            //Allocates card array (the box of cards I guess)
+            //Adds all 52 cards to the deck
+            _cards = Deck.All;
+            
+            //Initially shuffles the deck
+            Shuffle();
+        }
+
+        //PROPERTIES
+        public static Card[] All
+        {
+            //This should probably be a readonly singleton to save memory... research later
+            get
+            {
+                var cards = new Card[NumCards];
+                for (int i = 0; i < NumCards; i++)
+                {
+                    cards[i] = new Card(i);
+                }
+                return cards;
+            }
+        }
+
+        //METHODS
+        public void Shuffle()
+        {
+            //It might be better to refactor this random object
+            Random r = new Random();
+            //Swaps every card with a random position (can be itself)
+            //Ensures virtually infinite entropy I think
+            for(int i = 0; i < NumCards; i++)
+            {
+                //Stores swapcard because random
+                int swap = r.Next(NumCards);
+
+                //Swap
+                Card temp = _cards[i];
+                _cards[i] = _cards[swap];
+                _cards[swap] = temp;
+            }
+            //Resets top of the deck to 0
+            _top = 0;
+        }
+        
+        public Card Draw()
+        {
+            //Returns top card, moves top position down one
+            if (_top < NumCards)
+            {
+                return _cards[_top++];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException("Out of cards error!");
+            }
+        }
+        
+        public override string ToString()
+        {
+            //Used for debugging
+            string allCards = "";
+            foreach (Card card in _cards)
+            {
+                allCards += card.ToString() + Environment.NewLine;
+            }
+            return allCards;
+        }
+
+        //PRIVATE FIELDS
+        //The array of cards
+        private Card[] _cards;
+        
+        //Keeps track of the top of the deck, like a stack
+        //Only the deck needs to know where this is
+        private int _top;
+    }
+
+}
---
+++ CribbageTest/Test.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using CribbageEngine;
+
+namespace CribbageTest
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(TestCribStrategies(5));
+        }
+
+        public static double TestCribStrategies(int n)
+        {
+            //This function compares the GREEDY throwing strategy with the much slower PERFECT PLAY strategy
+            //The strategies are compared n times and the percentage of success is returned
+
+            var deck = new Deck();
+            ThrowingHandAITest ai;
+
+            //Tracks trial successes vs. failures
+            int same = 0, different = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                //Gives AI new input
+                deck.Shuffle();
+                ai = new ThrowingHandAITest(deck, ThrowingHandAI.ThrowingStrategy.Invalid);
+
+                bool success = ai.CompareCribStrategies();
+
+                if (success) same++;
+                else different++;
+            }
+
+            //Returns percentage of trials that gave same result
+            return 100 * ((double) same / (same + different));
+        }
+
+        public static void PrintCards(Card[] cards)
+        {
+            foreach (var card in cards)
+            {
+                Console.WriteLine(card);
+            }
+        }
+
+        internal struct Result
+        {
+            public Result(string testName)
+            {
+                success = true;
+                this.testName = testName;
+            }
+            public bool? success;
+            public string testName;
+        }
+
+        internal delegate Result TestFunction();
+
+        internal static string GetTestName()
+        {
+            //Returns the Name of the Method who calls this function
+            return new System.Diagnostics.StackFrame(1).GetMethod().Name;
+        }
+    }
+
+    class DeckTest : Deck
+    {
+
+    }
+
+    class ThrowingHandTest : ThrowingHand
+    {
+        //CONSTRUCTOR
+        public ThrowingHandTest(Deck deck, int size = 6) : base(deck, size)
+        {
+
+        }
+
+        //RUN TEST BATTERY
+        public void RunTests()
+        {
+            var tests = new List<Test.TestFunction>();
+            tests.Add(TestValid);
+            tests.Add(TestUnmarkAll);
+            tests.Add(TestMark);
+
+            foreach (var test in tests)
+            {
+                var res = test();
+                var output = new StringBuilder();
+                output.Append(res.testName);
+                switch (res.success)
+                {
+                    case null:
+                        output.Append(" was inconclusive!");
+                        break;
+                    case true:
+                        output.Append(" passed!");
+                        break;
+                    case false:
+                        output.Append(" failed!");
+                        break;
+                }
+                Console.WriteLine(output.ToString());
+            }
+        }
+
+        //INDIVIDUAL TESTS
+
+        //TODO: THERE NEEDS TO BE A CHECK THROWN METHOD
+        private Test.Result TestValid()
+        {
+            var res = new Test.Result(Test.GetTestName());
+
+            if (_size == 6)
+            {
+                UnmarkAll();
+
+                if (Valid) res.success = false;
+                MarkThrown(0);
+
+                if (Valid) res.success = false;
+                MarkThrown(1);
+
+                //Should only be valid if you have thrown two cards away
+            }
+            else
+            {
+                res.success = null;
+            }
+            return res;
+        }
+        private Test.Result TestUnmarkAll()
+        {
+            var res = new Test.Result(Test.GetTestName());
+
+            if (_size == 6)
+            {
+                //Assume MarkThrown works
+                MarkThrown(0);
+                if (_numThrown != 1) res.success = null;
+                if (_thrown[0] != true) res.success = null;
+                MarkThrown(1);
+
+                UnmarkAll();
+
+                if (_numThrown != 0) res.success = false;
+
+                if (_thrown[0] != false) res.success = false;
+                if (_thrown[1] != false) res.success = false;
+            }
+            else
+            {
+                res.success = null;
+            }
+            return res;
+        }
+        private Test.Result TestMark()
+        {
+            var res = new Test.Result(Test.GetTestName());
+
+            if (_size == 6)
+            {
+                try
+                {
+                    //This should throw an exception, if it doesn't things are busted
+                    res.success = false;
+                    MarkThrown(100);
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    res.success = true;
+                }
+
+                UnmarkAll();
+
+                MarkThrown(0);
+                if (_numThrown != 1) res.success = false;
+                if (_thrown[0] != true) res.success = false;
+
+                MarkThrown(0);
+                if (_numThrown != 1) res.success = false;
+
+                MarkThrown(1);
+                if (_numThrown != 2) res.success = false;
+                if (_thrown[1] != true) res.success = false;
+            }
+            else
+            {
+                res.success = null;
+            }
+            return res;
+        }
+    }
+
+    class ThrowingHandAITest : ThrowingHandAI
+    {
+        public ThrowingHandAITest(Deck deck, ThrowingStrategy strategy) : base(deck, strategy)
+        {
+
+        }
+
+        public bool CompareCribStrategies()
+        {
+            //Overrides usual strategy with new one
+            var ignore = GetOptimalThrow(ThrowingStrategy.IgnoreCrib);
+            var optimize = GetOptimalThrow(ThrowingStrategy.DeoptimizeCrib);
+
+            if (Enumerable.SequenceEqual(ignore, optimize))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
