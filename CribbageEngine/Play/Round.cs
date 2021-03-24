4fa17b7b726cdc8cf7c4c7e65da7290a9dc96202
Chris Laforet <Chris.Laforet@shuttereleasefilms.com>
2021-03-24 15:30:48 -0400

    Adding initial deal for the round - not considering who is dealer yet

--- CribbageEngine/Play/Round.cs
+++ CribbageEngine/Play/Round.cs
@@ -8,8 +8,29 @@ namespace CribbageEngine.Play
 {
 	public class Round
 	{
-		public Round(Game game) => this.Game = game;
+		public const int INITIAL_DEAL_CARD_COUNT = 6;
+		public const int PER_PLAYER_CRIB_CARD_COUNT = 2;
+		private Deck _deck = new Deck();
+		private IList<Card> _crib = new List<Card>();
+
+		public Round(Game game)
+		{
+			this.Game = game;
+			this._deck.Shuffle();
+		}
 
 		public Game Game { get; private set; }
+
+		public void Start()
+		{
+			IList<Player> players = Game.Players;
+			for (int count = 0; count < INITIAL_DEAL_CARD_COUNT; count++)
+			{
+				foreach (Player player in Game.Players)
+				{
+					player.AddCard(_deck.Draw());
+				}
+			}
+		}
 	}
 }
--- UnitTests/RoundTests.cs
+++ UnitTests/RoundTests.cs
@@ -21,5 +21,20 @@ namespace UnitTests
 
 			Assert.AreEqual(game, round.Game);
 		}
+
+		[Test]
+		public void givenANewRound_whenStartIsInvoked_thenPlayersReceive6CardsEach()
+		{
+			Game game = new Game();
+			game.RegisterPlayer(new Player(PlayerTests.PLAYER1_NAME));
+			game.RegisterPlayer(new Player(PlayerTests.PLAYER2_NAME));
+
+			Round round = game.Start();
+			round.Start();
+
+			foreach (Player player in game.Players) {
+				Assert.AreEqual(Round.INITIAL_DEAL_CARD_COUNT, player.GetHand().Count());
+			}
+		}
 	}
 }
